6aa22a8c71bd9487cb50bce6d9b5f4655ec2031d
Victor Koronen <koronen@kth.se>
2011-11-07 23:37:19 +0100

    Install rspec

---
+++ spec/lib/personnummer_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'personnummer'
+
+describe Personnummer do
+  xit "works"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+
+RSpec.configure do |config|
+end
