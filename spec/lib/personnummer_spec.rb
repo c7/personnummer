a39db76a7ad5831b851401cdc7590cfdd19dd133
Victor Koronen <koronen@kth.se>
2011-11-07 23:59:11 +0100

    Add tests for #initialize

--- spec/lib/personnummer_spec.rb
+++ spec/lib/personnummer_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 require 'personnummer'
 
 describe Personnummer do
+  describe "initialize" do
+    it "accepts a valid personnummer" do
+      expect{ Personnummer.new(8304285912) }.not_to raise_error(ArgumentError)
+      expect{ Personnummer.new('830428-5912') }.not_to raise_error(ArgumentError)
+      expect{ Personnummer.new('050428+5912') }.not_to raise_error(ArgumentError)
+    end
+
+    it "throws ArgumentError on an invalid personnummer" do
+      expect{ Personnummer.new(nil) }.to raise_error(ArgumentError)
+      expect{ Personnummer.new(17) }.to raise_error(ArgumentError)
+      expect{ Personnummer.new('112233-4455') }.to raise_error(ArgumentError)
+    end
+  end
+
   describe "age" do
     xit "works" do
     end
