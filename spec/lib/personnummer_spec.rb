1f7f3b4aa95b7f1385e4b0f2c742050fdb471e95
Victor Koronen <koronen@kth.se>
2011-11-08 00:49:36 +0100

    Add tests for most methods

--- spec/lib/personnummer_spec.rb
+++ spec/lib/personnummer_spec.rb
@@ -1,12 +1,19 @@
+# -*- encoding: utf-8 -*-
 require 'spec_helper'
 require 'personnummer'
 
 describe Personnummer do
   describe "initialize" do
-    it "accepts a valid personnummer" do
-      expect{ Personnummer.new(8304285912) }.not_to raise_error(ArgumentError)
-      expect{ Personnummer.new('830428-5912') }.not_to raise_error(ArgumentError)
-      expect{ Personnummer.new('050428+5912') }.not_to raise_error(ArgumentError)
+    it "accepts a valid personnummer (with control digit)" do
+      expect{ Personnummer.new(9001010017) }.not_to raise_error(ArgumentError)
+      expect{ Personnummer.new('900101-0017') }.not_to raise_error(ArgumentError)
+      expect{ Personnummer.new('100101+0017') }.not_to raise_error(ArgumentError)
+    end
+
+    it "accepts a valid personnummer (without control digit)" do
+      expect{ Personnummer.new(900101001) }.not_to raise_error(ArgumentError)
+      expect{ Personnummer.new('900101-001') }.not_to raise_error(ArgumentError)
+      expect{ Personnummer.new('100101+001') }.not_to raise_error(ArgumentError)
     end
 
     it "throws ArgumentError on an invalid personnummer" do
@@ -22,37 +29,98 @@ describe Personnummer do
   end
 
   describe "born" do
-    xit "works" do
+    it "returns the correct date" do
+      Personnummer.new(900101001).born.should == Date.parse("1990-01-01")
     end
   end
 
   describe "region" do
-    xit "works" do
+    it "returns '' for persons born on or after 1991-01-01" do
+      Personnummer.new(910101001).region.should == ''
+    end
+
+    it "returns the correct region for persons born before 1991-01-01" do
+      it_maps_range_to_region 000..139, 'Stockholms Län'
+      it_maps_range_to_region 140..159, 'Uppsala län'
+      it_maps_range_to_region 160..189, 'Södermanlands län'
+      it_maps_range_to_region 190..239, 'Östergötlands län'
+      it_maps_range_to_region 240..269, 'Jönköpings län'
+      it_maps_range_to_region 270..289, 'Kronobergs län'
+      it_maps_range_to_region 290..319, 'Kalmar län'
+      it_maps_range_to_region 320..329, 'Gotlands län'
+      it_maps_range_to_region 330..349, 'Blekinge län'
+      it_maps_range_to_region 350..389, 'Kristianstads län'
+      it_maps_range_to_region 390..459, 'Malmöhus län'
+      it_maps_range_to_region 460..479, 'Hallands län'
+      it_maps_range_to_region 480..549, 'Göteborgs och Bohus län'
+      it_maps_range_to_region 550..589, 'Älvsborgs län'
+      it_maps_range_to_region 590..619, 'Skaraborgs län'
+      it_maps_range_to_region 620..649, 'Värmlands län'
+      it_maps_range_to_region 650..659, 'Födda utomlands'
+      it_maps_range_to_region 660..689, 'Örebro län'
+      it_maps_range_to_region 690..709, 'Västmanlands län'
+      it_maps_range_to_region 710..739, 'Kopparbergs län'
+      it_maps_range_to_region 750..779, 'Gävleborgs län'
+      it_maps_range_to_region 780..819, 'Västernorrlands län'
+      it_maps_range_to_region 820..849, 'Jämtlands län'
+      it_maps_range_to_region 850..889, 'Västerbottens län'
+      it_maps_range_to_region 890..929, 'Norrbottens län'
+      it_maps_range_to_region 930..999, 'Födda utomlands eller utländska medborgare födda i Sverige'
     end
   end
 
   describe "control_digit" do
-    xit "works" do
+    it "responds with the right control digit" do
+      Personnummer.new(9001010017).control_digit.should == 7
+      Personnummer.new('900101-0017').control_digit.should == 7
+      Personnummer.new('900101-001').control_digit.should == 7
     end
   end
 
   describe "to_s" do
-    xit "works" do
+    it "uses the standard string representation" do
+      Personnummer.new(9001010017).to_s.should == '900101-0017'
+      Personnummer.new('900101-0017').to_s.should == '900101-0017'
+      Personnummer.new('900101-001').to_s.should == '900101-0017'
     end
   end
 
   describe "valid?" do
-    xit "works" do
+    it "marks a valid personnummer with control digit as valid" do
+      Personnummer.new('900101-0017').should be_valid
+    end
+
+    it "marks a valid personnummer without control digit as invalid" do
+      Personnummer.new('900101-001').should_not be_valid
+    end
+
+    it "throws ArgumentError on an invalid personnummer" do
+      expect{ Personnummer.new('112233-4455') }.to raise_error(ArgumentError)
     end
   end
 
-  describe "male?" do
-    xit "works" do
+  describe "gender" do
+    it "recognizes males" do
+      Personnummer.new('900101-001').should be_male
+      Personnummer.new('900101-003').should be_male
+      Personnummer.new('900101-005').should be_male
+      Personnummer.new('900101-007').should be_male
+      Personnummer.new('900101-009').should be_male
+    end
+
+    it "recognizes females" do
+      Personnummer.new('900101-000').should be_female
+      Personnummer.new('900101-002').should be_female
+      Personnummer.new('900101-004').should be_female
+      Personnummer.new('900101-006').should be_female
+      Personnummer.new('900101-008').should be_female
     end
   end
 
-  describe "female?" do
-    xit "works" do
-    end
+private
+
+  def it_maps_range_to_region(range, region)
+    Personnummer.new('901231-%03d' % range.first).region.should == region
+    Personnummer.new('901231-%03d' % range.last).region.should == region
   end
 end
