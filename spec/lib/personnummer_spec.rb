18b1d107a0e6b476959de1abd13a02eaa7cb4c72
Victor Koronen <koronen@kth.se>
2011-11-07 23:53:29 +0100

    Expand the spec stub

--- spec/lib/personnummer_spec.rb
+++ spec/lib/personnummer_spec.rb
@@ -2,5 +2,43 @@ require 'spec_helper'
 require 'personnummer'
 
 describe Personnummer do
-  xit "works"
+  describe "age" do
+    xit "works" do
+    end
+  end
+
+  describe "born" do
+    xit "works" do
+    end
+  end
+
+  describe "region" do
+    xit "works" do
+    end
+  end
+
+  describe "control_digit" do
+    xit "works" do
+    end
+  end
+
+  describe "to_s" do
+    xit "works" do
+    end
+  end
+
+  describe "valid?" do
+    xit "works" do
+    end
+  end
+
+  describe "male?" do
+    xit "works" do
+    end
+  end
+
+  describe "female?" do
+    xit "works" do
+    end
+  end
 end
